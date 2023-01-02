85d3da6a38a1b6ae2ef26f2c0bea94b170dbeb02
Ashok Timilsina <ashok@timilsina.com>
2023-01-01 22:57:05 -0600

    c

--- app.py
+++ app.py
@@ -313,14 +313,18 @@ def delete_article(article_id):
 # FLASK APP ROUTE BELOW HERE
 
 @app.route('/')
-def index_page():
-    """Renders html template that includes some JS - NOT PART OF JSON API!"""
-    category = Category.query.all()
-    article = Article.query.limit(41).all()
-    source = Source.query.limit(33).all()
-    c=Category.query.filter_by(name="general").first()
-    catart = c.category_article
-    return render_template('homepage.html', article=article, category=category, source=source, catart=catart)
+def base_page():
+    return render_template('/base.html')
+
+# @app.route('/')
+# def index_page():
+#     """Renders html template that includes some JS - NOT PART OF JSON API!"""
+#     category = Category.query.all()
+#     article = Article.query.limit(41).all()
+#     source = Source.query.limit(33).all()
+#     c=Category.query.filter_by(name="general").first()
+#     catart = c.category_article
+#     return render_template('homepage.html', article=article, category=category, source=source, catart=catart)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
