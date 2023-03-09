23f75c0367b079427d879bece9ddb2fc09dee1ec
Ashok Timilsina <ashok@timilsina.com>
2023-03-09 01:43:11 -0600

    change baseurl in app.js '

--- static/app.js
+++ static/app.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://web-production-2c27.up.railway.app/";
-const BASE_URL = "http://127.0.0.1:5000";
+const BASE_URL = "https://web-production-2c27.up.railway.app/";
+// const BASE_URL = "http://127.0.0.1:5000";
 
 
 $(document).on('click', '#iBtn', function(e){ 
